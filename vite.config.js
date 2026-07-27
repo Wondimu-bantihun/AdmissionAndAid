1c68dbb13cff5da53ed4a6fd04bf1d74028382f3
wondimu bantihun <75518995+Wondimu-bantihun@users.noreply.github.com>
2026-07-27 06:59:30 -0700

    Update vite.config.js
    repository name AdmissionAndAid is added in the base

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'AdmissionAndAid',
   plugins: [
     react(),
     tailwindcss(),
   ],
 })
+
+
